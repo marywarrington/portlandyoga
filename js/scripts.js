627d6d3a0c5dfe5d0f4bc6121aa7b233f2f5a140
schuylermk <schuyler.m.k@gmail.com>
2016-02-02 08:54:29 -0800

    Add js for session info

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,50 @@
-var Location = function(studioName, quadrant, streetAddress, website) {
-  this.studioName = studioName;
-  this.quadrant = quadrant;
-  this.streetAddress = streetAddress;
-  this.website = website;
+var Session = function(studio, level, style, className, instructor) {
+  this.studio = studio;
+  this.level = level;
+  this.style = style;
+  this.className = className;
+  this.instructor = instructor;
 }
 
-var Time = function(day, hour) {
-  this.day = day;
-  this.hour = hour;
+Session.prototype.displayInfo = function() {
+  return this.studio + ", " + this.className + ", " + this.instructor;
 }
-
-var SessionsArray = function(newArray) {
-    this.newArray = [];
-}
-
-var sessionsArray = [
-  {
-    Location.studioName: "Yoga Union",
-    Location.quadrant: "SE",
-    style: "Yin",
-    Time.day:"Tuesday",
-    Time.hour: 13
-  },
-  {
-    Location.studioName: "Yoga Pearl",
-    Location.quadrant: "NW",
-    style: "Vinyasa",
-    Time.day:"Wednesday",
-    Time.hour: 15
-  },
-  {
-    Location.studioName: "Yoga on Yamhill",
-    Location.quadrant: "SW",
-    style: "Hatha",
-    Time.day:"Friday",
-    Time.hour: 9
-  },
-  {
-    Location.studioName: "Love Hive Yoga",
-    Location.quadrant: "NE",
-    style: "Yin",
-    Time.day:"Sunday",
-    Time.hour: 18
-  },
-];
+// var Time = function(day, hour) {
+//   this.day = day;
+//   this.hour = hour;
+// }
+//
+// var SessionsArray = function(newArray) {
+//     this.newArray = [];
+// }
+//
+// var sessionsArray = [
+//   {
+//     Location.studioName: "Yoga Union",
+//     Location.quadrant: "SE",
+//     style: "Yin",
+//     Time.day:"Tuesday",
+//     Time.hour: 13
+//   },
+//   {
+//     Location.studioName: "Yoga Pearl",
+//     Location.quadrant: "NW",
+//     style: "Vinyasa",
+//     Time.day:"Wednesday",
+//     Time.hour: 15
+//   },
+//   {
+//     Location.studioName: "Yoga on Yamhill",
+//     Location.quadrant: "SW",
+//     style: "Hatha",
+//     Time.day:"Friday",
+//     Time.hour: 9
+//   },
+//   {
+//     Location.studioName: "Love Hive Yoga",
+//     Location.quadrant: "NE",
+//     style: "Yin",
+//     Time.day:"Sunday",
+//     Time.hour: 18
+//   },
+// ];
--- specs/specs.js
+++ specs/specs.js
@@ -1,24 +1,45 @@
-describe('Location', function() {
-  it("create an object with the appropriate properties", function() {
-    var testLocation = new Location("Yoga Union", "SE", "2305 SE 50th Ave", "http://www.yogaunioncwc.com");
-    expect(testLocation.studioName).to.equal("Yoga Union");
-    expect(testLocation.quadrant).to.equal("SE");
-    expect(testLocation.streetAddress).to.equal("2305 SE 50th Ave");
-    expect(testLocation.website).to.equal("http://www.yogaunioncwc.com");
+describe('Session', function() {
+  it('will create an object with session properties', function() {
+    var newSession = new Session("Yoga Union", "beginner", "yin", "Yin Yoga", "Dave Fink");
+    expect(newSession.studio).to.equal("Yoga Union");
+    expect(newSession.level).to.equal("beginner");
+    expect(newSession.style).to.equal("yin");
+    expect(newSession.className).to.equal("Yin Yoga");
+    expect(newSession.instructor).to.equal("Dave Fink");
+  });
+
+  it('will return the abbreviated class information', function() {
+    var newSession = new Session("Yoga Union", "beginner", "yin", "Yin Yoga", "Dave Fink");
+    expect(newSession.displayInfo()).to.equal("Yoga Union, Yin Yoga, Dave Fink");
   });
 });
 
-describe('Time', function() {
-  it("create an object with the appropriate properties", function() {
-    var testTime = new Time("Tuesday", 15);
-    expect(testTime.day).to.equal("Tuesday");
-    expect(testTime.hour).to.equal(15);
-  });
-});
 
-describe('SessionsArray', function() {
-  it("will create an array with the given values", function() {
-  var testSessionsArray = new SessionsArray([]);
-  expect(testSessionsArray.newArray).to.eql([]);
-  });
-});
+
+
+
+
+// describe('Location', function() {
+//   it("create an object with the appropriate properties", function() {
+//     var testLocation = new Location("Yoga Union", "SE", "2305 SE 50th Ave", "http://www.yogaunioncwc.com");
+//     expect(testLocation.studioName).to.equal("Yoga Union");
+//     expect(testLocation.quadrant).to.equal("SE");
+//     expect(testLocation.streetAddress).to.equal("2305 SE 50th Ave");
+//     expect(testLocation.website).to.equal("http://www.yogaunioncwc.com");
+//   });
+// });
+//
+// describe('Time', function() {
+//   it("create an object with the appropriate properties", function() {
+//     var testTime = new Time("Tuesday", 15);
+//     expect(testTime.day).to.equal("Tuesday");
+//     expect(testTime.hour).to.equal(15);
+//   });
+// });
+//
+// describe('SessionsArray', function() {
+//   it("will create an array with the given values", function() {
+//   var testSessionsArray = new SessionsArray([]);
+//   expect(testSessionsArray.newArray).to.eql([]);
+//   });
+// });
